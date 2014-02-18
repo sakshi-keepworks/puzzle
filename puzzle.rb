0a7709bc9546a70bcae254e911ab901ad9b664a0
sjain1107 <sakshijain1107@gmail.com>
2014-02-18 23:58:26 +0530

    Multiple handles

--- puzzle.rb
+++ puzzle.rb
@@ -8,7 +8,10 @@ require 'thin'
 set :server, 'unicorn'
 
 class MyCache
-  def initialize()
+
+  attr_reader :name
+  def initialize(name)
+    @name = name
     @mutex = Mutex.new
     @last_update = DateTime.new            # by default, -4732 BC
     @client = Twitter::REST::Client.new do |config|
@@ -24,31 +27,52 @@ class MyCache
       if DateTime.now - @last_update > 10.0 / (3600 * 24)
         @last_update = DateTime.now
 
-        arr = []
-        retweeters = @client.retweeters_of(433267305544302593)
+        tweet = @client.user_timeline(@name)[0]
+
+        @arr = []
+        retweeters = @client.retweeters_of(tweet.id)
 
         retweeters.each do |retweeter|
           ob = {}
           ob[:profile_image] = retweeter.profile_image_url
           ob[:followers_count] = retweeter.followers_count
-          arr.push(ob)
+          @arr.push(ob)
         end
-
-        # remove the duplicates and sort on the users with the most followers,
-        sorted_influencers = arr.sort_by { |hsh| hsh[:followers_count] }
-        sorted_influencers.reverse!
-        @cache = sorted_influencers[0..9]
+        
+        @cache = influencers(@arr)
+      
       end
-
       @cache
     end
     @cache
   end
+
+      def influencers(array)
+        if @name == "BillGates" ||"firefox" || "twitter" || "github" || "timorielly" || "gvanrossum"
+          sorted_influencers = @arr.first(10).sort_by { |hsh| hsh[:followers_count] }
+        elsif @name == "dhh"
+          sorted_influencers = @arr[9..19].sort_by { |hsh| hsh[:followers_count] }
+        elsif @name == "martinfowler"
+          sorted_influencers = @arr.first(9).sort_by { |hsh| hsh[:followers_count] }
+        elsif @name == "spolsky"
+          sorted_influencers = @arr.sort_by { |hsh| hsh[:followers_count] }
+        end
+        sorted_influencers.reverse!
+        if @name == "martinfowler"
+          @cache = sorted_influencers[0..8]
+        else
+        @cache = sorted_influencers[0..9]
+        end
+        @cache
+      end
 end
 
-my_cache = MyCache.new
-
 get '/' do
-  @cache = my_cache.get_cache
   erb :index
+end
+
+get '/:name' do |n|
+  my_cache = MyCache.new(n)
+  @cache = my_cache.get_cache
+  erb :"#{n}"
 end
